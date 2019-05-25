ad8827e001d61ae3b5d63998ef2d817726deb3bd
Kacper Dondziak <maniakrzelaza@gmail.com>
2019-05-25 17:28:05 +0200

    add partial menu

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     def index
-
+        @posts = Post.all.order("created_at DESC")
     end
 
     def new
@@ -11,10 +11,27 @@ class PostsController < ApplicationController
         @post = Post.new(post_params)
 
         if @post.save
-            redirect_to 'new'
+            redirect_to :action => "show", :id =>  @post.id
         else
             render 'new'
         end
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def update
+
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
     end
 
     private
